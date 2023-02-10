46b839c973215384e1c27762ac7d5f9bc57fc89b
rberman6 <rittuberman@gmail.com>
2023-02-10 13:39:11 -0700

    updated mobile nav menu

--- script.js
+++ script.js
@@ -9,4 +9,9 @@ hamburgerIcon.addEventListener("click", function (e) {
     // console.log("clicked");
     // 3. locate the default ul mobile menu, and create active class on DOM, toggle method applied on active class.
     document.querySelector(".mobile").classList.toggle("active");
+    // 4. when the hamburger icon is clicked the active class will appear and h1, and h2 will be removed on DOM
+    const mainHeading = document.querySelector('h1');
+    mainHeading.remove();
+    const secondaryHeading = document.querySelector('h2');
+    secondaryHeading.remove();
 });
