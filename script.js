eaf9254da5a89b88904bb9b7342b87276bda83c7
rberman6 <rittuberman@gmail.com>
2023-02-13 17:16:26 -0700

    upaated html files

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // HAMBURGER MENU FOR RESPONSIVE DESIGN - PSEUDO CODE
 
-// 1. locate the hamburger icon 
+// 1. locate the hamburger icon and save in a variable
 const hamburgerIcon = document.querySelector("#hamburgerMenu")
 // const mobileNav = document.querySelector(".active")
 
-// 2. listen for clicks on hamburger icon
+// 2. listen for clicks on hamburger icon using eventlistener
 hamburgerIcon.addEventListener("click", function (e) {
     // console.log("clicked");
     // 3. locate the default ul mobile menu, and create active class on DOM, toggle method applied on active class.
